cd75a4e26cbca174ad54478de740877c7e3a7a1a
Horst <arielhorst@gmail.com>
2023-10-08 16:32:11 -0300

    First commit

---
+++ src/main/java/com/dianome/app/DianomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.dianome.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DianomeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dianome/app/DianomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dianome.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
