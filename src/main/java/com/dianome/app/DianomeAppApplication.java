a15c02ee15eb602d06083591bbfc50b408660cf5
Horst <arielhorst@gmail.com>
2023-10-08 19:26:48 -0300

    Criação do package Entregador.

--- src/main/java/com/dianome/app/DianomeAppApplication.java
+++ src/main/java/com/dianome/app/DianomeAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DianomeAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DianomeAppApplication.class, args);
 	}
 
---
+++ src/main/java/com/dianome/app/entregador/EntregadorController.java
@@ -0,0 +1,26 @@
+package com.dianome.app.entregador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/entregador")
+
+
+public class EntregadorController {
+
+
+
+
+
+
+
+
+
+
+
+
+}
